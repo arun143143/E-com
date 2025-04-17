b653a0857f0b78c12e20e0ad3cd40665e3aabe7a
arun143143 <arunprasathpuni289@gmail.com>
2025-04-17 13:02:41 +0530

    cheacking urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,7 +7,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path('signup/', SignupView.as_view(), name='signup'),
+    
     path('login/', LoginView.as_view(), name='login'),
     path('protected/', ProtectedView.as_view(), name='protected'),
     path('refresh-token/', RefreshTokenView.as_view(), name='refresh_token'),
